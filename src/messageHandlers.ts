4399e0006824787cac9172f95d64dfa482244bb8
Francis Usher <francis.w.usher@gmail.com>
2020-11-15 21:07:10 -0800

    Factor out the sound test and showcase handlers

--- src/messageHandlers.ts
+++ src/messageHandlers.ts
@@ -9,40 +9,46 @@ async function handleMessage(msg: Message, client: Client): Promise<void> {
     }
 
     if (msg.guild?.id === config.FORTIES_GUILD) {
-        if (msg.content.includes(config.FORTIES_SHOWCASE)) {
-            if (msg.attachments.size > 0) {
-                msg.attachments.map(async (each) => {
-                    const url = each.proxyURL;
-                    const attachment = new MessageAttachment(url);
+        await handleShowcaseMessage(msg, client);
+        await handleSoundtestMessage(msg, client);
+    }
+}
 
-                    console.log(
-                        `40s channel posted showcase: ${msg.author.username} ${url}`
-                    );
-
-                    const showcaseChannel = (await client.channels.fetch(
-                        config.FORTIES_SHOWCASE
-                    )) as TextChannel;
-                    await showcaseChannel.send(
-                        `Posted by: ${msg.author.toString()}`,
-                        attachment
-                    );
-                });
+async function handleShowcaseMessage(msg: Message, client: Client) {
+    if (msg.content.includes(config.FORTIES_SHOWCASE) && msg.attachments.size > 0) {
+        const showcaseChannel = (await client.channels.fetch(
+            config.FORTIES_SHOWCASE
+        )) as TextChannel;
+        await Promise.all(msg.attachments.map(
+            async (each) => {
+                const url = each.proxyURL;
+                const attachment = new MessageAttachment(url);
+                console.log(
+                    `40s channel posted showcase: ${msg.author.username} ${url}`
+                );
+                await showcaseChannel.send(
+                    `Posted by: ${msg.author.toString()}`,
+                    attachment
+                );
+                return;
             }
-        }
-        if (msg.content.includes(config.FORTIES_SOUNDTEST)) {
-            const url = msg.content.split(`<#${config.FORTIES_SOUNDTEST}> `)[1];
+        ))
 
-            console.log(
-                `40s channel posted soundtest: ${msg.author.username} ${url}`
-            );
+    }
+}
 
-            const soundTestChannel = (await client.channels.fetch(
-                config.FORTIES_SOUNDTEST
-            )) as TextChannel;
-            await soundTestChannel.send(
-                `Posted by: ${msg.author.toString()}\n${url}`
-            );
-        }
+async function handleSoundtestMessage(msg: Message, client: Client) {
+    if (msg.content.includes(config.FORTIES_SOUNDTEST)) {
+        const url = msg.content.split(`<#${config.FORTIES_SOUNDTEST}> `)[1];
+        console.log(
+            `40s channel posted soundtest: ${msg.author.username} ${url}`
+        );
+        const soundTestChannel = (await client.channels.fetch(
+            config.FORTIES_SOUNDTEST
+        )) as TextChannel;
+        await soundTestChannel.send(
+            `Posted by: ${msg.author.toString()}\n${url}`
+        );
     }
 }
 
