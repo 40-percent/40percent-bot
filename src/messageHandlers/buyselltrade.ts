6b0bb9bbe3092a607b69f519f6018b63e72bfa02
Jake Happersett <jake.happersett@gmail.com>
2020-12-13 20:54:45 -0500

    Update buyselltrade.ts
    Clean up interval code, ensure that pending intervals and timeouts are cleared when bst message is corrected

--- src/messageHandlers/buyselltrade.ts
+++ src/messageHandlers/buyselltrade.ts
@@ -1,20 +1,27 @@
-import { Message, Client, User, TextChannel, MessageEmbed } from "discord.js";
+import { Message, Client, User, TextChannel, MessageEmbed, GuildChannel, DMChannel, NewsChannel } from "discord.js";
+import { join } from "path";
 import { clearTimeout } from "timers";
 import config from '../config';
 
-class BstPost {
-    user: User;
-    message: Message;
-    messageText: string;
+class BstPost extends Message {
     validationMessages: string[];
-    // only set when validation fails 
+    // folowing only set when validation fails 
     warningMessage?: Message;
     expiration?: NodeJS.Timeout;
 
     constructor(msg: Message, warningMsg?: Message, expiration?: NodeJS.Timeout) {
-        this.user = msg.author;
-        this.message = msg;
-        this.messageText = msg.content;
+        super(msg.client, {
+            id: msg.id,
+            type: msg.type,
+            content: msg.content,
+            author: msg.author,
+            pinned: msg.pinned,
+            tts: msg.tts,
+            embeds: msg.embeds,
+            attachments: msg.attachments,
+            nonce: msg.nonce
+        }, msg.channel);
+
         this.validationMessages = [];
         this.warningMessage = warningMsg;
         this.expiration = expiration;
@@ -23,7 +30,11 @@ class BstPost {
         this.validationMessages.push(errText);
     }
     setMessageExpiration(timeInMs: number) {
-        this.expiration = setTimeout(() => this.message.delete(), timeInMs);
+        //this.expiration = 
+        setTimeout(this.deleteMessage.bind(this), timeInMs);
+    }
+    deleteMessage() {
+        this.delete().then(() => this.warningMessage?.edit(this.warningMessage.content + "/n **Post was not edited in 30 minutes and was deleted**"));
     }
     clearMessageExpiration() {
         if (this.expiration)
@@ -55,7 +66,7 @@ async function validateBstMessage(bstPost: BstPost) {
 }
 
 async function handleFailedBstValidation(bstPost: BstPost) {
-    const channel = bstPost.message.guild?.channels.cache.get(config.FORTIES_BST_DISCUSSION);
+    const channel = bstPost.guild?.channels.cache.get(config.FORTIES_BST_DISCUSSION);
     // this is wonky for sure - discordJS returns a type "GuildChannel" on  
     // lookup so we have to cast to a TextChannel to send any messages, see : 
     // https://stackoverflow.com/questions/53563862/send-message-to-specific-channel-with-typescript/53565548
@@ -63,41 +74,44 @@ async function handleFailedBstValidation(bstPost: BstPost) {
 
     const warningMsg = await channel.send(createEmbededFailMessage(bstPost));
 
-    await runDeleteCountDown(bstPost, warningMsg);
+    runDeleteCountDown(bstPost, warningMsg);
 }
 
-async function runDeleteCountDown(bstPost: BstPost, warningMessage: Message) {
+function runDeleteCountDown(bstPost: BstPost, warningMessage: Message) {
     // start a delete timeout for 30 minutes 
-    bstPost.setMessageExpiration(1000 * 60 * 30);
+    bstPost.setMessageExpiration(1000);
     // check message every 10 seconds
-    setInterval(() => checkForEdit(bstPost, warningMessage), 1000 * 10);
+    const interval: NodeJS.Timeout = setInterval(() => checkForEdit(bstPost, warningMessage, interval), 1000 * 10);
 }
 
-function checkForEdit(bstPost: BstPost, warningMessage: Message) {
-    if (bstPost.message.deleted) return;
+function checkForEdit(bstPost: BstPost, warningMessage: Message, interval: NodeJS.Timeout) {
+    if (bstPost.deleted) return;
 
-    const originalStamp = bstPost.message.createdTimestamp;
-    
-    bstPost.message.fetch().then(msg => (msg.editedTimestamp ?? originalStamp > originalStamp) ? validateBstMessage(new BstPost(msg, warningMessage, bstPost.expiration)) : null);
-    // todo: update the warning message 
+    bstPost.fetch()
+        .then(msg => {
+            if (msg.editedTimestamp ?? bstPost.createdTimestamp > bstPost.createdTimestamp) {
+                clearInterval(interval);
+                validateBstMessage(new BstPost(msg, warningMessage, bstPost.expiration));
+            }
+        });
 }
 
 function createEmbededFailMessage(post: BstPost): MessageEmbed {
     const embed = {
-        "description": `<@${post.user}>
+        "description": `${post.author}
         
         Your post does not meet the requirements for posting in Buy/Sell/Trade.
         
-        Please edit your message to correct the following errors:
+        Please edit your message to correct the following errors **in the next 30 minutes** or your post will be deleted:
         
-        ${post.validationMessages.map(x => "• " + x + "\n").toString()}
+        ${post.validationMessages.map(x => "• " + x + "\n").join("").toString()}
         
         `,
         "color": 12729122,
         "fields": [
             {
                 "name": "Original Mesasge Content",
-                "value": `${post.messageText}`
+                "value": `${post.content}`
             }
         ]
     };
@@ -106,16 +120,16 @@ function createEmbededFailMessage(post: BstPost): MessageEmbed {
 }
 
 function validateLocationBlockExistance(post: BstPost): boolean {
-    if (post.messageText.includes('[') && post.messageText.includes(']')) {
+    if (post.content.includes('[') && post.content.includes(']')) {
         return true;
     }
-    post.addMessage("The size of the location text is outside of the acceptable bounds, location should be abbreviated and no larger than 10 characters");
+    post.addMessage("Post is missing location block or does not use square brackets to wrap the location block (e.g. [US-VA])");
     return false;
 }
 
 function validateLocationBlockLength(post: BstPost) {
-    const locationBlock = parseLocation(post.messageText);
-    if (locationBlock.length < 2 && locationBlock.length > 10) {
+    const locationBlock = parseLocation(post.content);
+    if (locationBlock.length < 2 || locationBlock.length > 10) {
         post.addMessage("The size of the location text is outside of the acceptable bounds, location should be abbreviated and no larger than 10 characters");
     }
 }
