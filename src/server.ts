4907ea8c7c68dd99b46e27e919638e4891707258
Francis Usher <francis.w.usher@gmail.com>
2020-12-21 01:10:19 -0800

    Add remaining steps to the project request flow
    Closes 40-percent/40percent-bot#3, closes 40-percent/40percent-bot#4

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,39 @@
 import config from './config.js';
-import { Message, Client } from 'discord.js';
-import { handleMessage as handleMessageAsync } from './handlers';
+import {
+  Message,
+  Client,
+  MessageReaction,
+  User,
+  PartialUser,
+} from 'discord.js';
+import {
+  handleMessage as handleMessageAsync,
+  handleReaction as handleReactionAsync,
+} from './handlers';
 
-const client = new Client({ partials: ['REACTION'] });
+const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
 client.on('ready', () => {
   console.log('==== READY ====');
 });
 
-// We use this wrapper because typescript eslint will throw a fit if we return
-//  and unresolved promise instead of void in the client message handler.
+// We use these wrappers because typescript eslint will throw a fit if we return
+//  and unresolved promise instead of void in the client handlers.
 function handleMessageSyncWrapper(msg: Message): void {
   void handleMessageAsync(msg, client);
 }
 
+// We use these wrappers because typescript eslint will throw a fit if we return
+//  and unresolved promise instead of void in the client handlers.
+function handleReactionSyncWrapper(
+  reaction: MessageReaction,
+  user: User | PartialUser
+): void {
+  void handleReactionAsync(reaction, user, client);
+}
+
 client.on('message', handleMessageSyncWrapper);
+
+client.on('messageReactionAdd', handleReactionSyncWrapper);
 
 void client.login(config.BOT_TOKEN);
