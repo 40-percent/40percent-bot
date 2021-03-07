ac08af50759afd17f0da7140afc9a0d08d208b22
btmccollum <brad.mccollum@hey.com>
2021-03-07 11:52:58 -0600

    feat(soundtest): add url and attachment validation to soundtest

--- src/handlers/soundtest.ts
+++ src/handlers/soundtest.ts
@@ -1,16 +1,40 @@
-import { Message, Client, TextChannel } from 'discord.js';
+import { Message, MessageAttachment, Client, TextChannel } from 'discord.js';
 import config from '../config';
+import containsValidUrl from '../utils/containsValidUrl';
 
 export default async function handleSoundtestMessage(
   msg: Message,
   client: Client
 ): Promise<void> {
-  if (msg.content.includes(config.FORTIES_SOUNDTEST)) {
-    const url = msg.content.split(`<#${config.FORTIES_SOUNDTEST}> `)[1];
-    console.log(`40s channel posted soundtest: ${msg.author.username} ${url}`);
+  if (
+    msg.content.includes(config.FORTIES_SOUNDTEST) &&
+    msg.channel.id !== config.FORTIES_SOUNDTEST
+  ) {
     const soundTestChannel = (await client.channels.fetch(
       config.FORTIES_SOUNDTEST
     )) as TextChannel;
-    await soundTestChannel.send(`Posted by: ${msg.author.toString()}\n${url}`);
+    const filteredUserInput = msg.content
+      .replace(new RegExp(`<#${config.FORTIES_SOUNDTEST}>`), '')
+      .trim();
+    const validUrlPresent = containsValidUrl(filteredUserInput);
+    const attachmentUrl = msg.attachments?.first()?.url;
+
+    if (validUrlPresent || !!attachmentUrl) {
+      let messageToSend = `Posted by: ${msg.author.toString()}`;
+      if (filteredUserInput) messageToSend += `\n${filteredUserInput}`;
+      console.log(
+        `40s channel posted soundtest: ${msg.author.username} ${filteredUserInput}`
+      );
+      if (attachmentUrl) {
+        const attachment = new MessageAttachment(attachmentUrl);
+        await soundTestChannel.send(messageToSend, attachment);
+      } else {
+        await soundTestChannel.send(messageToSend);
+      }
+    } else {
+      await msg.reply(
+        'sound tests must contain a valid url and/or attachment.'
+      );
+    }
   }
 }
