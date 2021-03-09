2db44600fb8b8a68af17e9a84e2010e00439d72b
TJ <tom.campie@gmail.com>
2021-03-09 08:07:38 -0600

    Add reminder to mods to organize IC channels by alphabetical order
    Adds message to the bot-shenanegans channel for the IC approver to reorder the IC section in alphabetical order

--- src/handlers/project/create.ts
+++ src/handlers/project/create.ts
@@ -56,7 +56,7 @@ async function createProjectRole(
     config.BOT_COMMANDS_CHANNEL
   )) as TextChannel;
   await botCommandsChannel.send(
-    `<@${reviewerId}> please enter \`?addrank ${roleName}\` to create the project rank`
+    `<@${reviewerId}> please enter \`?addrank ${roleName}\` to create the project rank, and remember to organize channels by alphabetical order`
   );
   return role;
 }
