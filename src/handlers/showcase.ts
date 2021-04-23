5b6580fb689c7ac34b668811300b1862c250d5d2
Evan Ronnei <evanronnei@gmail.com>
2021-04-22 22:11:42 -0400

    Showcase embed channel names, logging, and missing URL prevention (#43)

--- src/handlers/showcase.ts
+++ src/handlers/showcase.ts
@@ -13,27 +13,49 @@ export default async function handleShowcaseMessage(
       config.FORTIES_SHOWCASE
     )) as TextChannel;
 
-    const url = msg.attachments.first()?.proxyURL as string
-    console.log(
-      `40s channel posted showcase: ${msg.author.username} ${url}`
-    );
+    const url = msg.attachments.first()?.proxyURL as string;
+
+    if (!url) {
+      console.log('Missing showcase image:', {
+        author: msg.author.tag,
+        message_url: msg.url,
+      });
+
+      await msg.reply(
+        "something went wrong. We'll take a look, but in the meantime please ensure an image is attached and try again."
+      );
+
+      return;
+    }
+
+    const trimDescription = (content: string): string => {
+      const trimmedContent = content
+        .replace(`<#${config.FORTIES_SHOWCASE}>`, '')
+        .trim();
+      return trimmedContent.length > 512
+        ? `${trimmedContent.substring(0, 512).trim()}...`
+        : trimmedContent;
+    };
 
     const embed = new MessageEmbed()
       .setAuthor(
         msg.author.username,
         msg.author.avatarURL() ?? msg.author.defaultAvatarURL
       )
-      .setDescription(
-        msg.content.replace(
-          `<#${config.FORTIES_SHOWCASE}>`,
-          ''
-        ).trim().substring(0, 512)
-      )
+      .setDescription(trimDescription(msg.content))
       .addField('Original Message', `[Jump 🔗](${msg.url})`, true)
-      .setTimestamp()
-      .setImage(url);
+      .setImage(url)
+      .setFooter(`#${(msg.channel as TextChannel).name}`)
+      .setTimestamp();
 
-    await showcaseChannel.send(embed);
+    const embedMessage = await showcaseChannel.send(embed);
+
+    console.log('40s channel posted showcase:', {
+      author: msg.author.tag,
+      image_url: url,
+      message_url: embedMessage.url,
+    });
+
     return;
   }
 }
