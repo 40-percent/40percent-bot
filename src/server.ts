856cd9b6cf2eee062166fc8b5bdbedf3d35a4e67
Francis Usher <francis.w.usher@gmail.com>
2021-09-16 02:57:33 -0700

    fix: eslint error on void handler promises

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import config from './config.js';
-import { Client, User } from 'discord.js';
+import { Client, User, Message, MessageReaction } from 'discord.js';
 import handleShowcaseMessage from './handlers/showcase';
 import handleSoundtestMessage from './handlers/soundtest';
 import {
@@ -10,53 +10,61 @@ import {
 import fetchPartial from './utils/fetchPartial';
 import callHandlers from './utils/callHandlers.js';
 
-const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER', 'GUILD_MEMBER'] });
+function messageShouldBeHandled(msg: Message): boolean {
+  // Ignore messages from bots
+  // Ignore messages from DMs
+  return !msg.author.bot && msg.guild?.id === config.FORTIES_GUILD;
+}
+
+function reactionShouldBeHandled(reaction: MessageReaction, user: User) {
+  // Ignore reactions from bots
+  // Ignore reactions from DMs
+  return !user.bot && reaction.message.guild?.id === config.FORTIES_GUILD;
+}
+
+const client = new Client({
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER', 'GUILD_MEMBER'],
+});
 
 client.once('ready', () => {
   console.log('==== READY ====');
 });
 
 client.on('error', (err) => {
-  console.log('Uncaught error:', err)
+  console.log('Uncaught error:', err);
 });
 
-client.on('message', async (msg) => {
-  await fetchPartial(msg)
-
-  // Ignore messages from bots
-  // Ignore messages from DMs
-  if (msg.author.bot || msg.guild?.id !== config.FORTIES_GUILD) return;
-
-  await callHandlers(
-    handleShowcaseMessage(msg, client),
-    handleSoundtestMessage(msg, client),
-    handleIcGbRequestMessage(msg, client)
-  );
+client.on('message', (msg) => {
+  void fetchPartial(msg).then(() => {
+    if (messageShouldBeHandled(msg)) {
+      void callHandlers(
+        handleShowcaseMessage(msg, client),
+        handleSoundtestMessage(msg, client),
+        handleIcGbRequestMessage(msg, client)
+      );
+    }
+  });
 });
 
-client.on('messageReactionAdd', async (reaction, user) => {
-  await Promise.all([fetchPartial(reaction), fetchPartial(user)])
-
-  // Ignore reactions from bots
-  // Ignore reactions from DMs
-  if (user.bot || reaction.message.guild?.id !== config.FORTIES_GUILD) return;
-
-  await callHandlers(
-    handleIcGbReviewReaction(reaction, client, user as User),
-    handleProjectAnnouncementReaction(reaction, user as User, 'add')
-  );
+client.on('messageReactionAdd', (reaction, user) => {
+  void Promise.all([fetchPartial(reaction), fetchPartial(user)]).then(() => {
+    if (reactionShouldBeHandled(reaction, user as User)) {
+      void callHandlers(
+        handleIcGbReviewReaction(reaction, client, user as User),
+        handleProjectAnnouncementReaction(reaction, user as User, 'add')
+      );
+    }
+  });
 });
 
-client.on('messageReactionRemove', async (reaction, user) => {
-  await Promise.all([fetchPartial(reaction), fetchPartial(user)])
-
-  // Ignore reactions from bots
-  // Ignore reactions from DMs
-  if (user.bot || reaction.message.guild?.id !== config.FORTIES_GUILD) return;
-
-  await callHandlers(
-    handleProjectAnnouncementReaction(reaction, user as User, 'remove')
-  );
+client.on('messageReactionRemove', (reaction, user) => {
+  void Promise.all([fetchPartial(reaction), fetchPartial(user)]).then(() => {
+    if (reactionShouldBeHandled(reaction, user as User)) {
+      void callHandlers(
+        handleProjectAnnouncementReaction(reaction, user as User, 'remove')
+      );
+    }
+  });
 });
 
 void client.login(config.BOT_TOKEN);
