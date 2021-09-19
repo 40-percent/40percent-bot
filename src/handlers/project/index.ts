a45ab4452762c7e46fceb5737792a0a883044928
Francis Usher <francis.w.usher@gmail.com>
2021-09-19 03:36:37 -0700

    feat(project): improve approve/reject buttons

--- src/handlers/project/index.ts
+++ src/handlers/project/index.ts
@@ -3,11 +3,8 @@ import {
   Message,
   Client,
   TextChannel,
-  MessageReaction,
-  User,
   Guild,
   MessageAttachment,
-  PartialMessageReaction,
   MessageActionRow,
   MessageButton,
   ButtonInteraction,
@@ -54,7 +51,6 @@ async function handleIcGbRequestMessage(
           .setCustomId('rejectProjectReview')
           .setLabel('Reject')
           .setStyle('DANGER')
-          .setDisabled(true)
       );
       await reviewChannel.send({
         content: reviewMessage,
@@ -75,74 +71,110 @@ async function handleIcGbReviewInteraction(
   interaction: ButtonInteraction,
   client: Client
 ): Promise<void> {
-  // Only handle reactions in the IC/GB review channel
+  // Only handle interactions in the IC/GB review channel
   console.log(interaction);
-  if (
-    interaction.channelId !== config.IC_GB_REVIEW_CHANNEL ||
-    interaction.customId != 'approveProjectReview'
-  ) {
-    await interaction.reply('Received unknown reaction');
+  if (interaction.channelId !== config.IC_GB_REVIEW_CHANNEL) {
     return;
   }
-  const reviewer = interaction.user;
-  const attachmentUrls = [...interaction.message.attachments.values()].map(
-    (attachment) => attachment.url
-  );
-  if (attachmentUrls.length !== 2) {
-    await interaction.reply('Must include both an image and metadata.');
-    return;
-  }
-  const response = await axios.get<ProjectReviewParams>(attachmentUrls[1]);
-  const reviewParams = response.data;
   // Explicit narrowing because we know this will be a normal message
   // sent in the 40s guild
   const typedMessage = interaction.message as Message;
   const guild = typedMessage.guild as Guild;
-  const validParams = await ReviewParams.validate(reviewParams, guild);
-  if (validParams) {
-    await CreateProject.boilerplate(reviewParams, guild, reviewer, client);
-    await interaction.reply('Project approved: generated boilerplate');
-  } else {
-    await interaction.reply('Role or channel already exists.');
+  const reviewer = interaction.user;
+
+  if (interaction.customId === 'approveProjectReview') {
+    const attachmentUrls = [...interaction.message.attachments.values()].map(
+      (attachment) => attachment.url
+    );
+    if (attachmentUrls.length !== 2) {
+      await interaction.reply('Must include both an image and metadata.');
+      return;
+    }
+    const response = await axios.get<ProjectReviewParams>(attachmentUrls[1]);
+    const reviewParams = response.data;
+
+    const validParams = await ReviewParams.validate(reviewParams, guild);
+    if (validParams) {
+      await Promise.allSettled([
+        CreateProject.boilerplate(reviewParams, guild, reviewer, client),
+        interaction.reply({
+          content: 'Project approved: generated boilerplate',
+        }),
+        typedMessage.edit({
+          components: [
+            new MessageActionRow().addComponents(
+              new MessageButton()
+                .setCustomId('approveProjectReviewConfirmation')
+                .setLabel('Approved')
+                .setStyle('SUCCESS')
+                .setDisabled(true)
+            ),
+          ],
+        }),
+      ]);
+    } else {
+      await interaction.reply('Role or channel already exists.');
+    }
+  }
+
+  if (interaction.customId === 'rejectProjectReview') {
+    await Promise.allSettled([
+      interaction.reply({
+        content: 'Please let the user know why their project was rejected.',
+      }),
+      typedMessage.edit({
+        components: [
+          new MessageActionRow().addComponents(
+            new MessageButton()
+              .setCustomId('rejectProjectReviewConfirmation')
+              .setLabel('Rejected')
+              .setStyle('DANGER')
+              .setDisabled(true)
+          ),
+        ],
+      }),
+    ]);
   }
 }
 
-async function handleProjectAnnouncementReaction(
-  reaction: MessageReaction | PartialMessageReaction,
-  user: User,
-  action: 'add' | 'remove'
+async function handleProjectAnnouncementInteraction(
+  interaction: ButtonInteraction
 ): Promise<void> {
-  // Only handle reactions in the IC/GB review channel
-  if (reaction.message.channel.id !== config.IC_GB_ANNOUNCE_CHANNEL) {
+  const typedMessage = interaction.message as Message;
+  // Only handle interactions in the IC/GB announcement channel
+  if (typedMessage.channelId !== config.IC_GB_ANNOUNCE_CHANNEL) {
     return;
   }
-  // The :white_check_mark: emoji is to subscribe
-  if (reaction.emoji.name === '✅') {
-    const attachmentUrls = reaction.message.attachments.map(
-      (attachment) => attachment.url
-    );
-    const response = await axios.get<ProjectAnnouncementParams>(
-      attachmentUrls[1]
-    );
-    const projectParams = response.data;
-    const guild = reaction.message.guild as Guild;
-    const member = await guild.members.fetch(user.id);
-
-    switch (action) {
-      case 'add': {
-        await member.roles.add(projectParams.roleId);
-        break;
-      }
-      case 'remove': {
-        await member.roles.remove(projectParams.roleId);
-        break;
-      }
-    }
+  const attachmentUrls = typedMessage.attachments.map(
+    (attachment) => attachment.url
+  );
+  const response = await axios.get<ProjectAnnouncementParams>(
+    attachmentUrls[1]
+  );
+  const projectParams = response.data;
+  const guild = interaction.guild as Guild;
+  const member = await guild.members.fetch(interaction.user.id);
+  if (interaction.customId === 'joinProjectRole') {
+    await Promise.allSettled([
+      member.roles.add(projectParams.roleId),
+      interaction.reply({
+        content: `Joined role <@&${projectParams.roleId}>!`,
+        ephemeral: true,
+      }),
+    ]);
+  } else if (interaction.customId === 'leaveProjectRole') {
+    await Promise.allSettled([
+      member.roles.remove(projectParams.roleId),
+      interaction.reply({
+        content: `Left role <@&${projectParams.roleId}>!`,
+        ephemeral: true,
+      }),
+    ]);
   }
 }
 
 export {
   handleIcGbRequestMessage,
-  handleProjectAnnouncementReaction,
+  handleProjectAnnouncementInteraction,
   handleIcGbReviewInteraction,
 };
